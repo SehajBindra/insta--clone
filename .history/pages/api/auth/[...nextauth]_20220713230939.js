56f307ea97e2fd8c2c9377a10797461a5c7fadaa
sehaj bindra <sehajbindra1234@gmail.com>
2022-07-13 23:49:51 +0530

    next auth 6

--- .history/pages/api/auth/[...nextauth]_20220713230939.js
+++ .history/pages/api/auth/[...nextauth]_20220713230939.js
@@ -20,13 +20,6 @@ export default NextAuth({
   },
 
   callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
     async session({ session, token, user }) {
       session.user.username = session.user.name
         .split("")
